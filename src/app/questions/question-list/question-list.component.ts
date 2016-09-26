a1fb37d1bdbc4b22b2433275d5948688775d5701
van <van@hovercloud.com>
2016-09-26 16:04:10 -0700

    updated the footer, got question list and question selection working better, cleaned up the background colors, added some additional dummy data

--- src/app/questions/question-list/question-list.component.ts
+++ src/app/questions/question-list/question-list.component.ts
@@ -8,12 +8,19 @@ import {Question} from '../question';
   templateUrl: './question-list.component.html'
 })
 export class QuestionListComponent implements OnInit {
-  questions: Question[] = [];
+  questions: Question[] = [
+    new Question('Are you a dummy?',
+      'Some employers will want to make sure you\'re not a dummy.  Experience with crash tests is not a positive.',
+      'http://assets.uvamagazine.org/images/uploads/2009/01-Spring/Research/Dummy.jpg'),
+    new Question('What do you think of jams made from raspberries?',
+      'This question might be asked whenever there are raspberries involved in the project.',
+      'http://kingofwallpapers.com/fruit-images/fruit-images-004.jpg'),
+    new Question('Are you from New Zealand?',
+      'Sometimes employers will want to know if you are from New Zealand, where people refer to themselves as' +
+      '"Kiwies".  Do you ever refer to yourself by naming a fruit?',
+      'http://www.greatgrubclub.com/domains/greatgrubclub.com/local/media/images/medium/4_1_1_kiwi.jpg')
+  ];
   @Output() questionSelected = new EventEmitter<Question>();
-  question = new Question('Dummy', 'Dummy Description',
-    'http://assets.uvamagazine.org/images/uploads/2009/01-Spring/Research/Dummy.jpg');
-  question2 = new Question('Dummy2', 'Dummy Description2',
-    'http://assets.uvamagazine.org/images/uploads/2009/01-Spring/Research/Dummy.jpg');
 
   constructor() { }
 
